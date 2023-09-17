bd49f6efb779ea283a3d31a90d4ed46357524514
Anagha <139119046+anaghaKP12@users.noreply.github.com>
2023-09-17 22:35:19 +0530

    knapsack.c

--- knapsack.c
+++ knapsack.c
@@ -1,70 +1,63 @@
-#include<stdio.h>
-#include<stdlib.h>
-int t[100][100],v[100],w[100],n,m,i,j,count;
+#include <stdio.h>
+#include <stdlib.h>
 
-int max(int a,int b)
-{
-	return (a>b)?a:b;
+int max(int a, int b){
+	return (a>b) ? a : b;
 }
-int knap(int n,int m)
-{
-    for(int i=0;i<n+1;i++)
-	{
-		for(j=0;j<m+1;j++)
-		{
-			if(i==0||j==0)
-				t[i][j]=0;
-            else{
-                count++;
-			if(j<w[i])
-				t[i][j]=t[i-1][j];
-			else
-				t[i][j]=max(t[i-1][j],v[i]+t[i-1][j-w[i]]);
-            }
-		}
+
+int t[100][100], v[100], w[100], n, m, i, j,count = 0;
+
+int knap(int i, int j){
+	if (t[i][j]==-1){
+		count++;
+		if (j<w[i])
+			t[i][j] = knap(i-1,j);
+		else
+			t[i][j] = max(knap(i-1,j),v[i]+knap(i-1,j-w[i]));
 	}
-    return t[n][m];
+	return t[i][j];
 }
-void main()
-{
-    for(int p=0;p<5;p++)
-    {
-        count = 0;
-        int ch;
-        printf("Enter the choice,press 0 to exit\n");
-        scanf("%d" ,&ch);
-        if(ch == 0)
-        break;
-	//int t[100][100],v[100],w[100],n,m,i,j;
-	printf("Number of items: ");
-	scanf("%d",&n);
-	printf("Sack capacity: ");
-	scanf("%d",&m);
 
-	printf("Weight\tValue\n");
-	for(i=1;i<n+1;i++)
+void main(){
+	for(int p = 0;p<5;p++)
 	{
+		int ch;
+		count = 0;
+		printf("Press 0 to exit else press 1 to continue\n");
+		scanf("%d" ,&ch);
+		if(ch ==0)
+		break;
+
+	printf("No. of Items>> ");
+	scanf("%d",&n);
+	printf("Sack Capacity>> ");
+	scanf("%d",&m);
+	
+	
+	printf("Weight\tValue\n");
+	for(i=1;i<n+1;i++){
 		scanf("%d\t%d",&w[i],&v[i]);
 	}
 	
-
-	printf("Max value %d\n",knap(n,m));
-	for(int i=0;i<n+1;i++)
-	{
-		for(int j=0;j<m+1;j++)
-			printf("%d ",t[i][j]);
-		printf("\n");
+	for(i=0;i<n+1;i++){
+		for(j=0;j<m+1;j++){
+			if (i==0||j==0)
+				t[i][j]=0;
+			else
+				t[i][j]=-1;
+		}
 	}
+	
+	printf("Maximum Value: %d\n",knap(n,m));
+	
 	printf("Composition:\n");
-	for(int i=n;i>0;i--)
-	{
-		if(t[i][m]!=t[i-1][m])
-		{
-			printf("%d\t",i);
-			m=m-w[i];
+	for(i=n;i>0;i--){
+		if (t[i][m] != t[i-1][m]){
+			printf("%d ",i);
+			m = m-w[i];
 		}
 	}
 	printf("\n");
-    printf("%d\n",count);
-    }
+	printf("%d\n",count);
+	}
 }
